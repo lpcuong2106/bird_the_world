0415d94d6f182e1dfba72dbe55d973f212c258b8
phucuongit <lpcuong2106@gmail.com>
2021-08-25 03:51:37 +0700

    init theme

---
+++ wp-content/themes/bird_the_world/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * bird_the_world Theme Customizer
+ *
+ * @package bird_the_world
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function bird_the_world_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'bird_the_world_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'bird_the_world_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'bird_the_world_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function bird_the_world_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function bird_the_world_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function bird_the_world_customize_preview_js() {
+	wp_enqueue_script( 'bird_the_world-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'bird_the_world_customize_preview_js' );
