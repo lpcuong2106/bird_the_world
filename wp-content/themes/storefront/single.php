af7df007605bbd18ba34d4828185cf1b837dd67f
unknown <thien1pro1@gmail.com>
2021-08-24 21:14:59 +0700

    test doi ui theme

--- wp-content/themes/storefront/single.php
+++ wp-content/themes/storefront/single.php
@@ -9,7 +9,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+		<p>Trang chi tiet bai viet</p>
 		<?php
 		while ( have_posts() ) :
 			the_post();
