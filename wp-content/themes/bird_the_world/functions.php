a1be27958e21ead5d8197f4c647db6f5edcf595e
phucuongit <lpcuong2106@gmail.com>
2021-08-25 08:10:31 +0700

    style header

--- wp-content/themes/bird_the_world/functions.php
+++ wp-content/themes/bird_the_world/functions.php
@@ -142,10 +142,13 @@ add_action( 'widgets_init', 'bird_the_world_widgets_init' );
 function bird_the_world_scripts() {
 	wp_enqueue_style( 'bird_the_world-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_enqueue_style('boostrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css', array(), _S_VERSION);
+	wp_enqueue_style('app-css',  get_template_directory_uri() . './assets/css/app.css', array(), _S_VERSION);
+
+
 
 	wp_enqueue_script( 'bird_the_world-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 	wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js', array(), _S_VERSION);
-	
+	wp_enqueue_script( 'iconify', 'https://code.iconify.design/2/2.0.3/iconify.min.js', array(), _S_VERSION);
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
--- wp-content/themes/bird_the_world/header.php
+++ wp-content/themes/bird_the_world/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,48 +13,73 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+	<style>
+		@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+	</style>
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'bird_the_world' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<header id="masthead" class="container">
+			<div class="site-branding">
+				<!-- <?php
+						the_custom_logo();
+						if (is_front_page() && is_home()) :
+						?>
+				<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
 				<?php
-			else :
+						else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
 				<?php
-			endif;
-			$bird_the_world_description = get_bloginfo( 'description', 'display' );
-			if ( $bird_the_world_description || is_customize_preview() ) :
+						endif;
+						$bird_the_world_description = get_bloginfo('description', 'display');
+						if ($bird_the_world_description || is_customize_preview()) :
 				?>
-				<p class="site-description"><?php echo $bird_the_world_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+				<p class="site-description"><?php echo $bird_the_world_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+											?></p>
+			<?php endif; ?> -->
+				<!-- remove -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bird_the_world' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+				<div class="row">
+					<div class="col-md-4">
+						<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+					</div>
+					<div class="col-md-8">
+						<nav>
+							<?php
+							wp_nav_menu(
+								array(
+									'theme_location' => 'menu-1',
+									'menu_id'        => 'primary-menu',
+									'menu_class'	=> 'top-menu',
+								)
+							);
+							?>
+						</nav>
+					</div>
+				</div>
+
+				<section class="intro_block" style="<?= 'background-image: url(' . get_template_directory_uri() . '/assets/img/bird_intro.png' . ')' ?>">
+					<div class="row">
+						<div class="col-md-6">
+							<h2 class="heading_title">
+								<b>Giúp bạn</b>
+								<br />tìm kiếm thông tin về loài chim
+							</h2>
+							<form id="header_search" method="get" action="<?php echo esc_url(home_url('/')); ?>">
+								<input type="text" class="search_bird" name="s" placeholder="Tìm kiếm..." value="<?php echo get_search_query(); ?>">
+								<button type="submit" class="search_bird_button" ><span class="iconify" data-icon="fluent:search-28-regular"></span></button>
+							</form>
+						</div>
+					</div>
+
+				</section>
+		</header><!-- #masthead -->
