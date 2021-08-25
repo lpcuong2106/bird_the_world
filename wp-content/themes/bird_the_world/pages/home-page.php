424e9d2cf4270da70c75e32a9d053d871d0cca0e
phucuongit <lpcuong2106@gmail.com>
2021-08-25 10:13:29 +0700

    step block

---
+++ wp-content/themes/bird_the_world/pages/home-page.php
@@ -0,0 +1,49 @@
+<section class="container home-page">
+    <div class="row">
+        <div class="col-md-6">
+            <img src="<?= get_template_directory_uri() . '/assets/img/bird_step.png' ?>" style="margin-left: -28px;"/>
+        </div>
+        <div class="col-md-6">
+            <h3 class="home-page_direct_step"><b>Các bước</b> thực hiện</h3>
+            <p class="home-page_direct_des">Hướng dẫn các thao tác tìm kiếm chim</p>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="step-one">
+                        <div class="step-one_icon">
+                            <span class="iconify" data-icon="fe:search"></span>
+                        </div>
+                        <b>Bước 1</b>
+                        <p>Nhập tên chim muốn tìm kiếm</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="step-one">
+                        <div class="step-one_icon">
+                        <span class="iconify" data-icon="bi:check-square"></span>
+                        </div>
+                        <b>Bước 2</b>
+                        <p>Tìm đúng chim bạn muốn tra cứu</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="step-one">
+                        <div class="step-one_icon">
+                        <span class="iconify" data-icon="bx:bx-book"></span>
+                        </div>
+                        <b>Bước 3</b>
+                        <p>Tìm đúng chim bạn muốn tra cứu</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="step-one">
+                        <div class="step-one_icon">
+                        <span class="iconify" data-icon="clarity:repeat-line"></span>
+                        </div>
+                        <b>Lặp lại</b>
+                        <p>Lặp lại các thao tác trên</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
